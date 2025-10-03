c57329cabb6daed56366f3148322db81172e0119
I5hanKumar <153290701+I5hanKumar@users.noreply.github.com>
2025-10-03 20:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+constructor(name, age){
+	this.name = name;
+	this.age = age;
 
-class Student extends Person {}
+	study(){
+		console.log(name + "is studying")
+	}
+	teach(){
+		console.log(name + "is teaching")
+	}
+}}
+get name(){
+	return this.name;
+}
 
-class Teacher extends Person {}
+set age(newage){
+	this.age = newage;
+}
+
+class Student extends Person {
+	const Person = new Person("John", 25);
+	console.log(Person.name);
+	console.log(Person.study);
+}
+
+class Teacher extends Person {
+	const Person = new Person("Alice", 22);
+	console.log(Person.name);
+}
 
 // Do not change the code below this line
 window.Person = Person;
